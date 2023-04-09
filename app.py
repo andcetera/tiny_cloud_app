efeddd1f953bb84384f6a3c74e8924598936c884
BeckyK <barimonsterbk@gmail.com>
2023-04-08 20:43:21 -0500

    successful test run model in flask

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 
 from flask import Flask, jsonify, render_template
-
 import json
-
-
+import pickle
 
 app = Flask(__name__)
 
@@ -11,8 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-
+@app.route('/predict')
+def predict():
+    with open('output/model.h5', 'rb') as file:
+        model = pickle.load(file)
+    predictions = model.predict([[3, 170, 64, 37, 225, 34.5, 0.356, 30]])
+    print(predictions)
+    return 'no errors!'
 
 if __name__ == "__main__":
     app.run(debug=True)
