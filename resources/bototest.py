0f56d2b924650ef5ace13c1a4abaea7b8fef2bfd
BeckyK <barimonsterbk@gmail.com>
2023-04-10 21:56:50 -0500

    delete bucket & clean up code for future use

--- resources/bototest.py
+++ resources/bototest.py
@@ -15,9 +15,14 @@ def create_bucket(prefix, s3_connection):
         print(name, region)
         return name, response
 
+# Create new bucket
 # s3_resource = boto3.resource('s3')
-# first_name, response = create_bucket(prefix='diabetes', s3_connection=s3_resource)
-# bucket_name = 'diabetesa762cc08-b755-4bdc-9c89-f0f1595f16ea'
+# bucket_name, response = create_bucket(prefix='diabetes', s3_connection=s3_resource)
+
+# Upload file to bucket
 # filename = 'diabetes.csv'
 # s3_resource.Object(bucket_name, filename).upload_file(Filename=filename)
 
+# Delete file & bucket
+# s3_resource.Object(bucket_name, filename).delete()
+# s3_resource.Bucket(bucket_name).delete()
