32515c85f3494cbab8f41d833100861ff6203f9c
BeckyK <barimonsterbk@gmail.com>
2023-04-10 21:50:33 -0500

    use boto3 to create bucket & add diabetes file

---
+++ resources/bototest.py
@@ -0,0 +1,23 @@
+import boto3
+import uuid
+
+def create_name(prefix):
+    return ''.join([prefix, str(uuid.uuid4())])
+
+def create_bucket(prefix, s3_connection):
+        session = boto3.session.Session()
+        region = session.region_name
+        name = create_name(prefix)
+        if region == 'us-east-1':
+            response = s3_connection.create_bucket(Bucket=name)
+        else:
+            response = s3_connection.create_bucket(Bucket=name, CreateBucketConfiguration={'LocationConstraint': region})
+        print(name, region)
+        return name, response
+
+# s3_resource = boto3.resource('s3')
+# first_name, response = create_bucket(prefix='diabetes', s3_connection=s3_resource)
+# bucket_name = 'diabetesa762cc08-b755-4bdc-9c89-f0f1595f16ea'
+# filename = 'diabetes.csv'
+# s3_resource.Object(bucket_name, filename).upload_file(Filename=filename)
+
