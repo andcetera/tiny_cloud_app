144c3e610bc2a0ce44fc79728ac07624f420aa8b
BeckyK <barimonsterbk@gmail.com>
2023-04-12 18:31:04 -0500

    moving test scaler

--- app.py
+++ app.py
@@ -13,13 +13,18 @@ def predict(vals):
     items = vals.split(',')
     vals = [float(i) for i in items]
 
+    with open('output/scaler.h5', 'rb') as f:
+        scaler = pickle.load(f)
+
     with open('output/model.h5', 'rb') as file:
         model = pickle.load(file)
     predictions = model.predict([vals])
 
     if predictions[0] == 1:
+        print(type(scaler))
         return jsonify('You have indicators for diabetes')
     else:
+        print(type(scaler))
         return jsonify('You do not have indicators for diabetes')
 
 if __name__ == "__main__":
